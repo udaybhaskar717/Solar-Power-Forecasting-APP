1018369b6ddaef7f72566d63be4df24d1cffc315
udaybhaskar717 <79568421+udaybhaskar717@users.noreply.github.com>
2023-03-02 11:48:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -76,10 +76,10 @@ def main():
     # get user inputs
     
         # Set page title and favicon
-    st.set_page_config(page_title="Solar Power Forecasting App", page_icon=":sunny:")
+#     st.set_page_config(page_title="Solar Power Forecasting App", page_icon=":sunny:")
 
     # Set header image
-    header_image = Image.open("header_image.jpg")
+    header_image = Image.open("GIL_Image.png")
     st.image(header_image, use_column_width=True)
 
     # Set author info
